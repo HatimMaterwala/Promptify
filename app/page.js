a9cde657e122e6e2d1957b8fea31f9ff86c359d2
Hatim Materwala <hatimmater1234@gmail.com>
2025-02-24 18:33:41 -0500

    Basic Layout Done

--- app/page.js
+++ app/page.js
@@ -2,8 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <main>
-        <h1>Home</h1>
-      </main>
+    <main className="max-w-[90vw] m-auto my-10">
+      <div className="homePage flex flex-col items-center justify-center gap-4">
+        <h1 className="text-6xl font-mono stroke-text font-bold bg-gradient-to-r from-blue-400 via-blue-500 to-pink-500 bg-clip-text text-transparent">
+          Promptify
+        </h1>
+        <h2 className="text-4xl text-center font-mono stroke-text my-2 opacity-40 mb-4">
+          Ignite Your Creativity with Inspiring Prompts
+        </h2>
+        <p className="text-2xl font-mono text-center w-[90%]">
+          Dive into a world of curated prompts designed to spark ideas, boost
+          productivity, and elevate your creative journey. Whether you're a
+          writer, artist, or innovator, find the perfect prompt to fuel your
+          next breakthrough.
+        </p>
+      </div>
+    </main>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import Image from 'next/image'
-
+"use client";
+import React from "react";
+import Image from "next/image";
+import { useState } from "react";
 const Navbar = () => {
-  return (
-    <nav className='flex justify-around items-center h-16 relative shadow-sm font-mono'>
-      <div className="logo">
-        <Image src={"/logo.png"} alt="Logo" width={50} height={50} />
-      </div>
-      <div className="buttons space-x-4">
-        <button className='bg-white text-black px-3 py-1 rounded-full font-bold border border-r-4 border-b-4 border-black'>Sign Out</button>
-        <button>Create Prompt</button>
-      </div>
-    </nav>
-  )
-}
+  const [isLogged, setIsLogged] = useState(false);
 
-export default Navbar
+  return (
+    <nav
+      className="z-10 m-auto sticky top-4 w-[90vw] flex rounded-md justify-between
+     px-6 items-center h-16 shadow-sm font-mono border border-r-4 border-b-4 border-black"
+    >
+      <div className="logo">
+        <Image
+          className="bg-blend-screen"
+          src={"/logo.png"}
+          alt="Logo"
+          width={50}
+          height={50}
+        />
+      </div>
+      {isLogged ? (
+        <div className="buttons space-x-4">
+          <button className="bg-white text-black px-3 py-1 rounded-full font-bold border border-r-4 border-b-4 border-black">
+            Sign Out
+          </button>
+          <button className="bg-white text-black px-3 py-1 rounded-full font-bold border border-r-4 border-b-4 border-black">
+            Create Prompt
+          </button>
+          <Image
+            className="bg-blend-screen inline"
+            src={"/logo.png"}
+            alt="Logo"
+            width={50}
+            height={50}
+          />
+        </div>
+      ) : (
+        <>
+          <button className="bg-white text-black px-3 py-1 rounded-full font-bold border border-r-4 border-b-4 border-black">
+            Sign In
+          </button>
+        </>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
