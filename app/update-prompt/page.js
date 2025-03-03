7f313419a9026c41d3223a2f6324b5d7c6c68089
Hatim Materwala <hatimmater1234@gmail.com>
2025-03-02 23:35:29 -0500

    Final Fixes - 2

--- app/update-prompt/page.js
+++ app/update-prompt/page.js
@@ -1,68 +1,71 @@
 "use client";
-import { useState , useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "../../components/Form";
 
-
-const edit = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams(); 
-  const promptId = searchParams.get("id");
-
+const EditPrompt = () => {
   const [submitting, setSubmitting] = useState(false);
+  const router = useRouter();
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
+  const [promptId, setPromptId] = useState(null);
 
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const id = searchParams.get("id");
+    setPromptId(id);
+  }, []);
 
-  useEffect(()=>{
-
+  useEffect(() => {
+    if (!promptId) return;
     const getPromptDetails = async () => {
-        const response = await fetch(`/api/prompt/${promptId}`);
-        const data = await response.json();
-        setPost({
-            prompt: data.prompt, 
-            tag: data.tag
-        });  
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
     };
-    
-    if(promptId) getPromptDetails();
-  },[promptId])
+    getPromptDetails();
+  }, [promptId]);
 
   const editPrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
-    if(!promptId) return alert("Prompt not found!!"); 
-    try{
-      const response = await fetch(`/api/prompt/${promptId}`,{
-        method: 'PATCH',
+    if (!promptId) return alert("Prompt not found!!");
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
         body: JSON.stringify({
           prompt: post.prompt,
-          tag: post.tag
+          tag: post.tag,
         }),
-      })
+      });
 
-      if(response.ok){
-        router.push('/');
+      if (response.ok) {
+        router.push("/");
       }
-    }catch(e){
+    } catch (e) {
       console.error(e);
-    }finally{
+    } finally {
       setSubmitting(false);
     }
   };
 
   return (
-      <Form 
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form
         type="Edit"
         post={post}
         setPost={setPost}
         submitting={submitting}
         handleSubmit={editPrompt}
       />
-  )
-}
+    </Suspense>
+  );
+};
 
-export default edit;
+export default EditPrompt;
